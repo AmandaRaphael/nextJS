c5f4ea409748192d8c9c6218ac72d8f758e8aad4
Amanda Maricia Raphael <amanda.maricia.raphael@ambrosys.de>
2023-09-03 20:23:44 +0200

    dynamic routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35e54a1f6ae85af2"; }
+/******/ 		__webpack_require__.h = function() { return "b2203736c024aaea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
